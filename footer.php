b578c6028a2b2b973b7c408388f262dd1cee1f11
bapk04 <ngogiakiet20012004@gmail.com>
2025-11-19 11:08:21 +0700

    fix footer.php

---
+++ footer.php
@@ -0,0 +1,71 @@
+<!-- Footer Section Begin -->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+
+            <!-- Giới thiệu -->
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="footer__about">
+                    <div class="footer__logo">
+                        <a href="#"><img src="img/logo.png" alt="Logo INOX"></a>
+                    </div>
+                    <p>Khách hàng là trọng tâm trong mô hình kinh doanh độc đáo của chúng tôi, bao gồm cả việc đặt vé phim trực tuyến.</p>
+                    <a href="#"><img src="img/payment.png" alt="Phương thức thanh toán"></a>
+                </div>
+            </div>
+
+            <!-- Menu -->
+            <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
+                <div class="footer__widget">
+                    <h6>Menu</h6>
+                    <ul>
+                        <li><a href="index.php">Trang Chủ</a></li>
+                        <li><a href="allmovie.php">Tất Cả Phim</a></li>
+                        <li><a href="about.php">Giới Thiệu</a></li>
+                        <li><a href="feedback.php">Phản Hồi</a></li>
+                        <li><a href="contact.php">Liên Hệ</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Thể loại -->
+            <div class="col-lg-2 col-md-3 col-sm-6">
+                <div class="footer__widget">
+                    <h6>Thể Loại</h6>
+                    <ul>
+                        <li><a href="allmovie.php">Hành Động</a></li>
+                        <li><a href="allmovie.php">Tâm Lý</a></li>
+                        <li><a href="allmovie.php">Hài Hước</a></li>
+                        <li><a href="allmovie.php">Lịch Sử</a></li>
+                        <li><a href="allmovie.php">Kinh Dị</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Địa chỉ -->
+            <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
+                <div class="footer__widget">
+                    <h6>ĐỊA CHỈ</h6>
+                    <div class="footer__newslatter">
+                        <p>Rạp INOX, Toà nhà 2, Tầng 4 Lotte Mart Nha Trang</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Bản quyền -->
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="footer__copyright__text">
+                    <p>Bản quyền ©
+                        <script>document.write(new Date().getFullYear());</script> - 2020
+                        Dự án phát triển bởi Gainam Gosalia Mo. 8460502720
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</footer>
+<!-- Footer Section End -->
